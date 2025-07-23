9c114ad4e84e14f2f1277f21faf7bf103022a203
srleom <shengrui.leom@gmail.com>
2025-07-23 21:34:07 +0800

    fix: remove Posthog reverse proxy
    - Changed PostHog API host to utilize the environment variable for dynamic configuration.
    - Removed hardcoded UI host to streamline integration with different environments.

--- next.config.ts
+++ next.config.ts
@@ -30,24 +30,6 @@ const nextConfig: NextConfig = {
 
     return config;
   },
-  async rewrites() {
-    return [
-      {
-        source: "/ingest/static/:path*",
-        destination: "https://us-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: "/ingest/:path*",
-        destination: "https://us.i.posthog.com/:path*",
-      },
-      {
-        source: "/ingest/decide",
-        destination: "https://us.i.posthog.com/decide",
-      },
-    ];
-  },
-  // Support PostHog trailing slash API requests
-  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
